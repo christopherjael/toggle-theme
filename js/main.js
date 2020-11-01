a90c1b3004ecae3758ea06162d8aa76d76ffe733
Christopher Montero <cristhofermontero1@gmail.com>
2020-11-01 19:18:15 -0400

    Second Commit

--- js/main.js
+++ js/main.js
@@ -1,18 +1,17 @@
-var btnTheme = document.getElementById('btn-toggle-theme');
+var btnTheme = document.getElementById("btn-toggle-theme");
 
-btnTheme.addEventListener('click', (e) => {
-   document.body.classList.toggle('dark-theme');
-   
-   if(document.body.classList.contains('dark-theme')){
-      btnTheme.innerHTML = `
+btnTheme.addEventListener("click", (e) => {
+  document.body.classList.toggle("dark-theme");
+
+  if (document.body.classList.contains("dark-theme")) {
+    btnTheme.innerHTML = `
       <i class="fas fa-sun"></i>
       Light Theme
-      `
-   }
-   else{
-      btnTheme.innerHTML = `
+      `;
+  } else {
+    btnTheme.innerHTML = `
       <i class="fas fa-moon"></i>
         Dark Theme
-      `
-   }
-})
+      `;
+  }
+});
