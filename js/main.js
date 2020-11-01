97696b4b508ab3e43cd9f9fdbad30e2e4b3f27ae
Christopher Montero <cristhofermontero1@gmail.com>
2020-11-01 18:51:16 -0400

    First Commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var btnTheme = document.getElementById('btn-toggle-theme');
+
+btnTheme.addEventListener('click', (e) => {
+   document.body.classList.toggle('dark-theme');
+   
+   if(document.body.classList.contains('dark-theme')){
+      btnTheme.innerHTML = `
+      <i class="fas fa-sun"></i>
+      Light Theme
+      `
+   }
+   else{
+      btnTheme.innerHTML = `
+      <i class="fas fa-moon"></i>
+        Dark Theme
+      `
+   }
+})
