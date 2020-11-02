7aef3c39be964cfec8e620d14f4dab5b2dfe11ad
Christopher Montero <cristhofermontero1@gmail.com>
2020-11-02 11:41:12 -0400

    3 Commit

--- js/main.js
+++ js/main.js
@@ -25,3 +25,33 @@ btnTheme.addEventListener("click", (e) => {
       `;
   }
 });
+
+var content = document.querySelector('.content');
+
+var images = [
+  'pexels-cottonbro-5026399',
+  'pexels-igor-costa-842793',
+  'pexels-nishant-aneja-2416481',
+  'pexels-pixabay-264337',
+  'pexels-roman-davayposmotrim-35990',
+  'pexels-snapwire-185364',
+  'pexels-the-th-179908',
+  'pexels-valdemaras-d-1874675',
+  'pexels-zami-2387327'
+];
+
+for (var i = 0; i < images.length; i++){
+  content.innerHTML += `
+  <div class="card">
+  <img src="./img/${images[i]}.jpg">
+
+  <div class="card-text">
+  <h2>Title</h2>
+  <p>Lorem Lorem Lorem Lorem Lorem Lorem Lorem</p>
+  
+  </div> 
+  </div>
+  `;
+};
+
+
